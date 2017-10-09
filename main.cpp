a0c48dfb74b6c0b13ea496f5cc46c7a124388321
malexkiy <malexkiy@gmail.com>
2017-10-09 11:49:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,21 +6,23 @@ auto find_two_elements_with_sum(Iterator first, Iterator last, T c)->std::pair<b
 {
 	std::pair<bool, std::pair<Iterator, Iterator>> found_pair;
 
-	while (*first < *last)
+	auto end = last - 1;
+
+	while (*first < *end)
 	{
-		T sum = *first + *last;
+		T sum = *first + *end;
 		if (sum == c)
 		{
 			found_pair.first = true;
 			found_pair.second.first = first;
-			found_pair.second.second = last;
+			found_pair.second.second = end;
 
 			return found_pair;
 		}
 		else
 		{
 			if (sum < c) first++;
-			else last--;
+			else end--;
 		}
 	}
 
@@ -31,7 +33,7 @@ int main()
 {
 	int arr[] = { -6, -4, 0, 2, 5, 7, 8, 8, 13, 17 };
 
-	auto found_pair = find_two_elements_with_sum(arr, arr + sizeof(arr)/sizeof(arr[0]) - 1, 5);
+	auto found_pair = find_two_elements_with_sum(arr, arr + sizeof(arr)/sizeof(arr[0]), 11);
 
 	return 0;
 }
